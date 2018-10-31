e0ca38f6f7a8bf4f7749dc233fb16ea059055869
Tao-Yi Lee <tylee@ieee.org>
2018-10-30 22:05:21 -0700

    initial commit

---
+++ inc/sensor_params.h
@@ -0,0 +1,13 @@
+/*
+ * sensor_params.h
+ *
+ *  Created on: Oct 29, 2018
+ *      Author: tylee
+ */
+
+#ifndef SENSOR_PARAMS_H_
+#define SENSOR_PARAMS_H_
+
+#define array_size 10000
+
+#endif /* SENSOR_PARAMS_H_ */
